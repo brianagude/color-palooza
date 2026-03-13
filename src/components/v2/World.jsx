dd13a6af448e52db0c33d8dad834b76db6ee4f5f
Briana Gude <briana.gude@gmail.com>
2026-03-13 17:15:06 -0400

    updated walls to be semi-transparent

--- src/components/v2/World.jsx
+++ src/components/v2/World.jsx
@@ -2,8 +2,9 @@
 
 import { Instance, Instances } from "@react-three/drei";
 import { useControls } from "leva";
-import { ryb2rgb } from 'rybitten';
+import { ryb2rgb } from "rybitten";
 import { cubes } from "rybitten/cubes";
+import * as THREE from "three";
 
 export default function World(args) {
   const { boxSize, resolution } = useControls({
@@ -22,103 +23,151 @@ export default function World(args) {
       joystick: "invertY",
     },
   });
-  
-  
+
   const curr_cube = cubes.get(args.preset).cube;
 
   const N = resolution;
   const cellsCount = N * N * N;
-  const landmarks = curr_cube.map(c => c.map(v => Math.round(v * (N - 1)) - (N-1)/2));
-  console.log(landmarks);
+  const landmarks = curr_cube.map((c) =>
+    c.map((v) => Math.round(v * (N - 1)) - (N - 1) / 2),
+  );
 
   /* From Google */
   const equalArrays = (arr1, arr2) => {
-    return arr1.length === arr2.length && arr1.every((element, index) => {
-      return element === arr2[index];
-    });
+    return (
+      arr1.length === arr2.length &&
+      arr1.every((element, index) => {
+        return element === arr2[index];
+      })
+    );
   };
 
-
   const positions = [...Array(cellsCount)].map((_, i) => {
-    const z = Math.floor(i / (N * N)) - (N-1)/2;
+    const z = Math.floor(i / (N * N)) - (N - 1) / 2;
     const r = i % (N * N);
-    const y = Math.floor(r / N) - (N-1)/2;
-    const x = r % N - (N-1)/2;
+    const y = Math.floor(r / N) - (N - 1) / 2;
+    const x = (r % N) - (N - 1) / 2;
 
     return [x, y, z];
   });
 
   const wallSize = N - 1;
   const half = (N - 1) / 2;
-  const wallOpacity = 0.05;
+  const wallOpacity = 1;
 
   return (
     <>
-      <Instances
-        limit={64000}
-        range={cellsCount}
-      >
+      <Instances limit={64000} range={cellsCount}>
         <meshBasicMaterial />
         <boxGeometry args={[boxSize.x, boxSize.y, boxSize.z]} />
 
         {(() => {
-
           return positions.map((position, i) => {
-            const isLandmark = landmarks.some(l => {
+            const isLandmark = landmarks.some((l) => {
               console.log(l, position);
               return equalArrays(l, position);
             });
 
-            const c1 = (position[0] + (N-1)/2) / (N-1);
-            const c2 = (position[1] + (N-1)/2) / (N-1);
-            const c3 = (position[2] + (N-1)/2) / (N-1);
+            const c1 = (position[0] + (N - 1) / 2) / (N - 1);
+            const c2 = (position[1] + (N - 1) / 2) / (N - 1);
+            const c3 = (position[2] + (N - 1) / 2) / (N - 1);
             const ryb = ryb2rgb([c1, c2, c3], { cube: curr_cube });
 
             const r = args.mode === "RYB" ? ryb[0] : c1;
             const g = args.mode === "RYB" ? ryb[1] : c2;
             const b = args.mode === "RYB" ? ryb[2] : c3;
-            const color = [r,g,b];
+            const color = [r, g, b];
 
             if (isLandmark && args.mode === "RYB-In-RGB") {
-              return <Instance key={i} position={position} color={color} scale={2} />;
+              return (
+                <Instance key={i} position={position} color={color} scale={2} />
+              );
             }
 
-            return <Instance key={i} position={position} color={color} scale={args.mode === "RYB-In-RGB" ? 0 : 1} />;
+            return (
+              <Instance
+                key={i}
+                position={position}
+                color={color}
+                scale={args.mode === "RYB-In-RGB" ? 0 : 1}
+              />
+            );
           });
-        })()
-        }
+        })()}
       </Instances>
 
       {/* Visual walls */}
       {/* Back wall */}
-        <mesh position={[0, 0, -half]}>
-          <boxGeometry args={[wallSize, wallSize, 0.1]} />
-          <meshStandardMaterial transparent opacity={wallOpacity} color="red" />
-        </mesh>
+      <mesh position={[0, 0, -half]} rotation-y={Math.PI}>
+        <planeGeometry args={[wallSize, wallSize]} />
+        <meshStandardMaterial
+          transparent
+          opacity={wallOpacity}
+          side={THREE.BackSide}
+          depthWrite={false}
+          color="red"
+        />
+      </mesh>
 
-        {/* Front wall */}
-        <mesh position={[0, 0, half]}>
-          <boxGeometry args={[wallSize, wallSize, 0.1]} />
-          <meshStandardMaterial transparent opacity={wallOpacity} color="green" />
-        </mesh>
+      {/* Front wall */}
+      <mesh position={[0, 0, half]}>
+        <planeGeometry args={[wallSize, wallSize]} />
+        <meshStandardMaterial
+          transparent
+          opacity={wallOpacity}
+          side={THREE.BackSide}
+          depthWrite={false}
+          color="green"
+        />
+      </mesh>
 
-        {/* Left wall */}
-        <mesh position={[-half, 0, 0]} rotation-y={Math.PI / 2}>
-          <boxGeometry args={[wallSize, wallSize, 0.1]} />
-          <meshStandardMaterial transparent opacity={wallOpacity} color="orange" />
-        </mesh>
+      {/* Left wall */}
+      <mesh position={[-half, 0, 0]} rotation-y={-Math.PI / 2}>
+        <planeGeometry args={[wallSize, wallSize]} />
+        <meshStandardMaterial
+          transparent
+          opacity={wallOpacity}
+          side={THREE.BackSide}
+          depthWrite={false}
+          color="orange"
+        />
+      </mesh>
 
-        {/* Right wall */}
-        <mesh position={[half, 0, 0]} rotation-y={Math.PI / 2}>
-          <boxGeometry args={[wallSize, wallSize, 0.1]} />
-          <meshStandardMaterial transparent opacity={wallOpacity} color="blue" />
-        </mesh>
+      {/* Right wall */}
+      <mesh position={[half, 0, 0]} rotation-y={Math.PI / 2}>
+        <planeGeometry args={[wallSize, wallSize]} />
+        <meshStandardMaterial
+          transparent
+          opacity={wallOpacity}
+          side={THREE.BackSide}
+          depthWrite={false}
+          color="blue"
+        />
+      </mesh>
 
-        {/* Floor */}
-        <mesh position={[0, -half, 0]} rotation-x={-Math.PI / 2} receiveShadow>
-          <boxGeometry args={[wallSize, wallSize, 0.1]} />
-          <meshStandardMaterial transparent opacity={wallOpacity} color="black" />
-        </mesh>
+      {/* Floor */}
+      <mesh position={[0, -half, 0]} rotation-x={Math.PI / 2}>
+        <planeGeometry args={[wallSize, wallSize]} />
+        <meshStandardMaterial
+          transparent
+          opacity={wallOpacity}
+          side={THREE.BackSide}
+          depthWrite={false}
+          color="purple"
+        />
+      </mesh>
+
+      {/* Top */}
+      <mesh position={[0, half, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[wallSize, wallSize]} />
+        <meshStandardMaterial
+          transparent
+          opacity={wallOpacity}
+          side={THREE.BackSide}
+          depthWrite={false}
+          color="yellow"
+        />
+      </mesh>
     </>
   );
 }
